00c705a6d5b51823217336d051be47be2042963e
visualstudio <visualstudio@122-PC11>
2019-04-10 11:20:35 -0400

    Section E

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -1,73 +1,75 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.IO;
-
 
 namespace ConsoleApp4
 {
     class Program
     {
         ArrayList Beowulf;
-        int counterletters = 0;
-        int countSpaces = 0;
+        int LineCount = 0;
         static void Main(string[] args)
-        {
-            Program a = new Program();
-            a.Beowulf = new ArrayList();
-            a.ReadTextFiles();
-        }
 
+        {
+            Program p = new Program();
+            p.Beowulf = new ArrayList();
+            p.ReadTextFiles();
+
+        }
         public void Run() { this.ReadTextFiles(); }
         public void ReadTextFiles()
         {
+            using (StreamReader file = new StreamReader(@"U:\Users\722784/Beowulf.txt"))
 
-            using (StreamReader sr = new StreamReader("U:/Users/722784/beowulf.txt"))
             {
-                string line;
                 int counter = 0;
-                int a = 0, myWord = 1;
-                int averageLetterPerWord;
 
-                while ((line = sr.ReadLine()) != null)
+                int number = 0;
+
+                string line;
+
+                while ((line = file.ReadLine()) != null)
+
                 {
-                    Console.WriteLine(line);
-                    Beowulf.Add(line);
-                    FindNumberOfBlankSpaces(line);
-                    counter++;
 
-                    while (a <= line.Length - 1)
+                    if (line.Contains("Sea") && line.Contains("Fare") || line.Contains("sea") && line.Contains("fare"))
+
                     {
-                        if (line[a] == ' ' || line[a] == '\n' || line[a] == '\t')
-                        {
-                            myWord++;
-                        }
-                        a++;
+
+                        int x = counter - 1;
+
+                        number++;
+
                     }
-                    a = 0;
+
+                    counter++;
 
                 }
 
-                averageLetterPerWord = counterletters / countSpaces;
+                Console.WriteLine($"The number of lines that contains *Sea* and *Fare* are {number}");
 
-                Console.WriteLine("\n\n\n\n********************************\nThe number of lines in the paragraph is " + counter);
-                Console.WriteLine("The number of words in paragraph is " + myWord);
-                Console.WriteLine("The number of average letters per word is  " + averageLetterPerWord);
-                Console.ReadLine();
+                file.Close();
+
             }
 
         }
+
+
         public int FindNumberOfBlankSpaces(string line)
         {
+            int countletters = 0;
+            int countSpaces = 0;
             foreach (char c in line)
             {
-                if (char.IsLetter(c)) { counterletters++; }
+                if (char.IsLetter(c)) { countletters++; }
                 if (char.IsWhiteSpace(c)) { countSpaces++; }
             }
             return countSpaces;
+
         }
     }
 }
