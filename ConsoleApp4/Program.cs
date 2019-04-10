0b22e306c6141d3919fb3b32d619f43150ac54c4
visualstudio <visualstudio@122-PC11>
2019-04-10 09:50:41 -0400

    Section A-1

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -3,13 +3,68 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
+using System.IO;
 
-namespace ConsoleApp4
+
+
+namespace ConsoleApp2
 {
     class Program
     {
+        ArrayList Beowulf;
+        int linecount = 0;
+
         static void Main(string[] args)
         {
+            Program p = new Program();
+            p.Beowulf = new ArrayList();
+            p.Run();
+
+        }
+
+        public void Run()
+        {
+            this.ReadTextFiles();
+        }
+        public void ReadTextFiles()
+        {
+            using (StreamReader file = new StreamReader("U:/Users/722784 /Beowulf.txt"))
+            {
+
+                int counter = 0;
+                string ln;
+                while ((ln = file.ReadLine()) != null)
+                {
+                    Console.WriteLine(ln);
+                    Beowulf.Add(ln);
+
+                }
+                file.Close();
+                Console.WriteLine($"File has{counter} lines");
+
+
+
+
+
+            }
+
+        }
+        public int FindNumberOfBlankSpaces(string line)
+        {
+            int countletters = 0;
+            int countSpaces = 0;
+            foreach (char c in line)
+            {
+                if (char.IsLetter(c)) { countletters++; }
+                if (char.IsWhiteSpace(c)) { countSpaces++; }
+
+            }
+            return countSpaces;
         }
     }
 }
+
+
+
+
